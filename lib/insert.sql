2a930784f2c1b7eac4230898caedbc5bbdeee526
Brian <brianhilsden@gmail.com>
2024-06-03 11:56:19 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,21,"M","black","high",TRUE);
